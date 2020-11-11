c25111000d2d2fef438016ea93ca82644c46287f
Stano Peťko <stano@stanopetko.eu>
2020-11-11 14:50:23 +0100

    Add some functions to PowerShell profile

--- computer/Microsoft.PowerShell_profile.ps1
+++ computer/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,26 @@ Import-Module oh-my-posh
 Set-Theme Paradox
 
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+function az-prod {
+    az account set --subscription {prod-subscription-id}
+    $account = az account show | ConvertFrom-Json
+    $account
+}
+function az-dev {
+    az account set --subscription {dev-subscription-id}
+    $account = az account show | ConvertFrom-Json
+    $account
+}
+function az-gabo {
+    az account set --subscription {gabo-subscription-id}
+    $account = az account show | ConvertFrom-Json
+    $account
+}
+
+function clean-build {
+    Write-Host "Deleting 'bin' folders..."
+    Get-ChildItem -Path . -Recurse -Filter "bin" -Directory | Remove-Item -Recurse
+    Write-Host "Deleting 'obj' folders..."
+    Get-ChildItem -Path . -Recurse -Filter "obj" -Directory | Remove-Item -Recurse
+}
